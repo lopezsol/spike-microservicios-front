ca996a5e23eec36143f72a012a505b4481815d07
lopezsol <mariasollopez23@gmail.com>
2025-08-29 09:42:37 -0300

    se cambia nombre componente

--- src/app/profile/components/profile-user/profile-user.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-profile-user',
-  imports: [],
-  templateUrl: './profile-user.component.html',
-  styleUrl: './profile-user.component.css',
-})
-export class ProfileUserComponent { }
