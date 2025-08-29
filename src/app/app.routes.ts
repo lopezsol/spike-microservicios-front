da19fdff46066fe630108c7cd24797b508d1d90e
lopezsol <mariasollopez23@gmail.com>
2025-08-29 09:32:09 -0300

    estrucutra basica del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { DataViewComponent } from './components/data-view/data-view.component';
 
 export const routes: Routes = [
     {
-        path: '', component: DataViewComponent
     }
 ];
---
+++ src/app/profile/components/profile-user/profile-user.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-user',
+  imports: [],
+  templateUrl: './profile-user.component.html',
+  styleUrl: './profile-user.component.css',
+})
+export class ProfileUserComponent { }
