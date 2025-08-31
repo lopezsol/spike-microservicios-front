09c55ca93c9cd9479263d8f4e3ce4c6b590a1e08
lopezsol <mariasollopez23@gmail.com>
2025-08-31 02:04:39 -0300

    se importa correctamente la interfaz province

--- src/app/profile/interfaces/user-response.interface.ts
+++ src/app/profile/interfaces/user-response.interface.ts
@@ -1,5 +1,6 @@
 import type { Locality } from '../../shared/interfaces/locality.interface';
 import type { Project } from '../../shared/interfaces/project.interface';
+import type { Province } from '../../shared/interfaces/province.interface';
 
 export interface UserResponse {
   id: number;
